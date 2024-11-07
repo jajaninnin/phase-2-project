ba33a99b61e0a83096ec7f0349bd2d731a18b049
Karina Mogha <moghakarina@gmail.com>
2024-11-07 15:14:15 -0500

    word changes 2

--- src/components /CupCakePage.js
+++ src/components /CupCakePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import CupCakeNewForm from "./CupecakeNewForm";
+import CupCakeNewForm from "./CupCakeNewForm";
 import CupCakeList from "./CupCake";
 import Search from "./Search";
 
