1d45780b50ebc8bcd9113be17595729adc7868c7
Karina Mogha <moghakarina@gmail.com>
2024-11-11 14:11:16 -0500

    css plus sorting finished

--- src/components/CupcakeList.js
+++ src/components/CupcakeList.js
@@ -4,27 +4,48 @@ import CupcakeCard from "./CupcakeCard";
 import Search from "./Search";
 
 function CupcakeList() {
-  const {cupcakes} = useOutletContext(); // coming from App
+  const { cupcakes } = useOutletContext();
   const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc"); 
 
-  const filteredCupcake = cupcakes.filter((cupcake) =>
-    cupcake.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
  
+  const handleSort = () => {
+    setSortOrder((prevSortOrder) => (prevSortOrder === "asc" ? "desc" : "asc"));
+  };
+
+ 
+  const filteredCupcake = cupcakes
+    .filter((cupcake) =>
+      cupcake.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a.price - b.price; 
+      } else {
+        return b.price - a.price; 
+      }
+    });
+
   return (
     <main>
       <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+      
+      <button className="submit-button" onClick={handleSort}>
+        Sort by Price: {sortOrder === "asc" ? "Low to High" : "High to Low"}
+      </button>
+
       <ul className="cards">
-      {filteredCupcake.map((cupcake) => (
-        <CupcakeCard
-          key={cupcake.id}
-          id={cupcake.id}
-          name={cupcake.name}
-          img={cupcake.image}
-          price={cupcake.price}
-        />
-      ))}
-    </ul>
+        {filteredCupcake.map((cupcake) => (
+          <CupcakeCard
+            key={cupcake.id}
+            id={cupcake.id}
+            name={cupcake.name}
+            img={cupcake.image}
+            price={cupcake.price}
+          />
+        ))}
+      </ul>
     </main>
   );
 }
