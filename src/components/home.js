c1d7d27cd634a894eaae2ba40d79b65ef24079a8
Karina Mogha <moghakarina@gmail.com>
2024-11-11 19:41:43 -0500

    added ai images

--- src/components/home.js
+++ src/components/home.js
@@ -14,8 +14,8 @@ function Home() {
       </p>
 
       <div className="shop-images">
-        <img src="https://i.pinimg.com/736x/27/2a/02/272a02a8f5ddb5b39c2151c2060ec644.jpg" alt="" />
-        <img src="https://i.pinimg.com/736x/a4/e2/d3/a4e2d361b506928c67e92defbbbd278b.jpg" alt="" />
+        <img src="https://files.oaiusercontent.com/file-vw3eShbtN2DfGHfkREqwcuZV?se=2024-11-12T00%3A42%3A55Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3De321779f-5263-49bd-a13f-b8286667590d.webp&sig=OPwOBcjoBIy8qavWKoduzYTozYlqQGSIRLoTZmwyoMY%3D" alt="" />
+        <img src="https://files.oaiusercontent.com/file-zk4xGjmk4iRzNyPiUDPOH7s4?se=2024-11-12T00%3A44%3A17Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D51abe5c2-bf6a-4488-9060-cc6d77f5a709.webp&sig=2J4kqPTv9hyje/N4OtiWW8jxkV05I9G2QbnbL6pKOes%3D" alt="" />
         <img src="https://i.pinimg.com/736x/23/f4/97/23f4975928a368442ae5104d074b5981.jpg" alt="" />
         <img src="https://i.pinimg.com/736x/74/80/cf/7480cf0f5a15b5e50e37e4a959d40ccf.jpg" alt="" />
       </div>
